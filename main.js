4ebc0cff6fdd9697c367912a841a20046eb338d7
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2024-11-01 23:59:40 +0100

    feat: add the ability for the user to select available AIs, as well as save new AIs

--- main.js
+++ main.js
@@ -1,32 +1,15 @@
-// main.js 
-// ChatGPT Electron 
-// by Andaroth 
+// main.js
+// ChatGPT Electron
+// by Andaroth
 // https://github.com/Andaroth/ai-assistant-electron
 
-const availableAIs = [
-  {
-    label: "ChatGPT",
-    url: 'https://chat.openai.com'
-  },
-  {
-    label: "Copilot",
-    url: 'https://copilot.microsoft.com/'
-  },
-  {
-    label: "MistralAI",
-    url: 'https://chat.mistral.ai/chat'
-  },
-  {
-    label: "Copilot Pro",
-    url: 'https://copilot.cloud.microsoft'
-  },
-]
+const availableAIs = require("./settings.json").availableAIs;
 
-const { Menu, app, BrowserWindow, session } = require('electron');
-const prompt = require('electron-prompt');
+const { Menu, app, BrowserWindow, session } = require("electron");
+const prompt = require("electron-prompt");
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 let win;
 let userSettings;
@@ -39,42 +22,50 @@ const defaultSettings = {
   assistant: "ChatGPT",
 };
 
-const userDataPath = app.getPath('userData');
-const configPath = path.join(userDataPath, 'config.json');
-const sessionFile = path.join(userDataPath, 'sessions.json');
+const userDataPath = app.getPath("userData");
+const configPath = path.join(userDataPath, "config.json");
+const sessionFile = path.join(userDataPath, "sessions.json");
 
-function changeAssistant(label,url, save = false, killCookies = true) {
+function changeAssistant(label, url, save = false, killCookies = true) {
   // kill cookies on AI change to prevent cookie errors:
-  if (killCookies) win.webContents.session.clearStorageData({ storages: ['cookies'] });
+  if (killCookies)
+    win.webContents.session.clearStorageData({ storages: ["cookies"] });
   win.loadURL(url);
   if (save) {
-    let currentSettings = Object.assign({}, userSettings || loadUserPreferences()); // mock
+    let currentSettings = Object.assign(
+      {},
+      userSettings || loadUserPreferences()
+    ); // mock
     const mutateConfig = Object.assign(currentSettings, { assistant: label }); // mutate
     userSettings = mutateConfig; // assign
-    fs.writeFileSync(configPath, JSON.stringify(userSettings), 'utf-8'); // save
+    fs.writeFileSync(configPath, JSON.stringify(userSettings), "utf-8"); // save
   }
 }
 
 function loadUserPreferences() {
   if (fs.existsSync(configPath)) {
-    const configFile = fs.readFileSync(configPath, 'utf-8');
+    const configFile = fs.readFileSync(configPath, "utf-8");
     userSettings = JSON.parse(configFile);
     return userSettings;
-  } else { // create config file if it does not exist
+  } else {
+    // create config file if it does not exist
     fs.writeFileSync(configPath, JSON.stringify(defaultSettings)); // create settings
-    return loadUserPreferences()
+    return loadUserPreferences();
   }
 }
 
 function changeUserTheme(name, reload = false) {
-  let currentSettings = Object.assign({}, userSettings || loadUserPreferences()); // mock
+  let currentSettings = Object.assign(
+    {},
+    userSettings || loadUserPreferences()
+  ); // mock
   const mutateConfig = Object.assign(currentSettings, { theme: name }); // mutate
   userSettings = mutateConfig; // assign
-  const configFile = path.join(app.getPath('userData'), 'config.json');
-  fs.writeFileSync(configFile, JSON.stringify(userSettings), 'utf-8'); // save
+  const configFile = path.join(app.getPath("userData"), "config.json");
+  fs.writeFileSync(configFile, JSON.stringify(userSettings), "utf-8"); // save
   const cssFile = path.join(userDataPath, name);
   if (fs.existsSync(cssFile)) {
-    const cssContent = fs.readFileSync(cssFile, 'utf8');
+    const cssContent = fs.readFileSync(cssFile, "utf8");
     win.webContents.insertCSS(cssContent);
   } else {
     fs.writeFileSync(cssFile, ""); // create empty theme
@@ -84,39 +75,45 @@ function changeUserTheme(name, reload = false) {
 }
 
 function toggleStreamer() {
-  let currentSettings = Object.assign({}, userSettings || loadUserPreferences()); // mock
-  const mutateConfig = Object.assign(currentSettings, { streamer: !currentSettings.streamer }); // mutate
+  let currentSettings = Object.assign(
+    {},
+    userSettings || loadUserPreferences()
+  ); // mock
+  const mutateConfig = Object.assign(currentSettings, {
+    streamer: !currentSettings.streamer,
+  }); // mutate
   userSettings = mutateConfig; // assign
-  const configFile = path.join(app.getPath('userData'), 'config.json');
-  fs.writeFileSync(configFile, JSON.stringify(userSettings), 'utf-8'); // save
-  win.reload()
+  const configFile = path.join(app.getPath("userData"), "config.json");
+  fs.writeFileSync(configFile, JSON.stringify(userSettings), "utf-8"); // save
+  win.reload();
 }
 
 function fetchThemes() {
-  const cssFiles = fs.readdirSync(userDataPath)
-    .filter(file => path.extname(file) === '.css')
-    .map(label => label);
+  const cssFiles = fs
+    .readdirSync(userDataPath)
+    .filter((file) => path.extname(file) === ".css")
+    .map((label) => label);
   return cssFiles || ["default.css"];
 }
 function getSessions() {
   if (fs.existsSync(sessionFile)) {
-    const sessions = JSON.parse(fs.readFileSync(sessionFile))
-    return sessions || {}
+    const sessions = JSON.parse(fs.readFileSync(sessionFile));
+    return sessions || {};
   } else {
-    fs.writeFileSync(sessionFile, '{}', 'utf-8');
+    fs.writeFileSync(sessionFile, "{}", "utf-8");
     return getSessions();
   }
 }
 
 function getSessionsNames() {
-  return Object.keys(getSessions() || {}) || []
+  return Object.keys(getSessions() || {}) || [];
 }
 
 function removeSession(name, session) {
   const mutableSession = getSessions() || {};
   if (mutableSession[name]) {
     delete mutableSession[name];
-    const sessionFile = path.join(app.getPath('userData'), 'sessions.json');
+    const sessionFile = path.join(app.getPath("userData"), "sessions.json");
     fs.writeFileSync(sessionFile, JSON.stringify(mutableSession));
     setTimeout(() => win.reload(), 1000);
   }
@@ -129,7 +126,7 @@ function storeSession(name, session) {
     session.cookies.get({}).then((cookies) => {
       // console.log('store, cookies', cookies);
       mutableSession[name] = { cookies };
-      const sessionFile = path.join(app.getPath('userData'), 'sessions.json');
+      const sessionFile = path.join(app.getPath("userData"), "sessions.json");
       fs.writeFileSync(sessionFile, JSON.stringify(mutableSession));
     });
   }
@@ -139,33 +136,36 @@ function loadSession(name, session) {
   // console.log('loadSession')
   const existingSessions = getSessions();
   const cookies = existingSessions[name]?.cookies || [];
-  const sessionFile = path.join(app.getPath('userData'), 'sessions.json');
+  const sessionFile = path.join(app.getPath("userData"), "sessions.json");
   if (fs.existsSync(sessionFile)) {
     session.clearStorageData();
     cookies.forEach((cookie) => {
       // console.log('load, cookie', cookie);
-      const url = `https://${cookie.domain.replace(/^\./, '')}`;
-      if (cookie.name.startsWith('__Secure-')) cookie.secure = true;  // flag safe
-      if (cookie.name.startsWith('__Host-')) {
-        cookie.secure = true;  // flag safe
-        cookie.path = '/';     // set root path
-        delete cookie.domain;  // delete, refer to url
+      const url = `https://${cookie.domain.replace(/^\./, "")}`;
+      if (cookie.name.startsWith("__Secure-")) cookie.secure = true; // flag safe
+      if (cookie.name.startsWith("__Host-")) {
+        cookie.secure = true; // flag safe
+        cookie.path = "/"; // set root path
+        delete cookie.domain; // delete, refer to url
         delete cookie.sameSite; // allow cookies from third auth
       }
-      session.cookies.set({
-        url,
-        name: cookie.name,
-        value: cookie.value,
-        domain: cookie.domain,
-        path: cookie.path,
-        secure: cookie.secure,
-        httpOnly: cookie.httpOnly,
-        expirationDate: cookie.expirationDate
-      }).then(() => {
-        // console.log(`${url} cookie ${cookie.name} restored`);
-      }).catch((error) => {
-        console.error('Error while opening cookie :', error);
-      });
+      session.cookies
+        .set({
+          url,
+          name: cookie.name,
+          value: cookie.value,
+          domain: cookie.domain,
+          path: cookie.path,
+          secure: cookie.secure,
+          httpOnly: cookie.httpOnly,
+          expirationDate: cookie.expirationDate,
+        })
+        .then(() => {
+          // console.log(`${url} cookie ${cookie.name} restored`);
+        })
+        .catch((error) => {
+          console.error("Error while opening cookie :", error);
+        });
     });
     win.reload();
   }
@@ -173,116 +173,217 @@ function loadSession(name, session) {
 
 function generateMenu() {
   const sessionMenuTemplate = [
-    ...(isMac ? [{
-      label: app.name,
-      submenu: [
-        { role: 'quit' }  // Cmd + Q pour quitter
-      ]
-    }] : []),
+    ...(isMac
+      ? [
+          {
+            label: app.name,
+            submenu: [
+              { role: "quit" }, // Cmd + Q pour quitter
+            ],
+          },
+        ]
+      : []),
     // Edition Menu
-    ...(isMac ? [{
-      label: 'Edit',
-      submenu: [
-        { role: 'undo' },
-        { role: 'redo' },
-        { type: 'separator' },
-        { role: 'cut' },
-        { role: 'copy' },
-        { role: 'paste' },
-        { role: 'selectAll' }
-      ]
-    }] : []),
+    ...(isMac
+      ? [
+          {
+            label: "Edit",
+            submenu: [
+              { role: "undo" },
+              { role: "redo" },
+              { type: "separator" },
+              { role: "cut" },
+              { role: "copy" },
+              { role: "paste" },
+              { role: "selectAll" },
+            ],
+          },
+        ]
+      : []),
     {
       label: "Change AI",
-      submenu: availableAIs.map(({ label, url }) => ({
-        label,
-        type: "checkbox",
-        checked: (userSettings.assistant || defaultSettings.assistant) === label,
-        click() {
-          changeAssistant(label,url,true);
-        }
-      })),
+      submenu: availableAIs
+        .filter(({ available }) => available)
+        .map(({ label, url }) => ({
+          label,
+          type: "checkbox",
+          checked:
+            (userSettings.assistant || defaultSettings.assistant) === label,
+          click() {
+            changeAssistant(label, url, true);
+          },
+        })),
+        
     },
     {
-      label: 'Sessions',
-      submenu: [...getSessionsNames().map((name) => ({
-        label: name,
-        click() {
-          loadSession(name, session.defaultSession);
-        }
-      })),
-      {
-        type: "separator"
-      },
-      {
-        label: "Save Current Session",
-        click: async () => {
-          const ask = () => {
-            prompt({
-              title: 'Saving Current Session',
-              label: 'Please chose a name:',
-              inputAttrs: {
-                type: 'text'
-              },
-              type: 'input'
-            }).then((text) => {
-              if (text === "") ask();
-              else if (text !== null) {
-                storeSession(text, session.defaultSession)
-                setTimeout(() => loadSession(text, session.defaultSession))
-              }
-            }).catch(console.error);
-          }
-          ask()
-        }
-      },
-      {
-        label: "Delete A Session",
-        click: async () => {
-          const ask = () => {
-            prompt({
-              title: 'Delete A Session',
-              label: 'Enter the EXACT NAME to remove:',
-              inputAttrs: {
-                type: 'text'
-              },
-              type: 'input'
-            }).then((text) => {
-              if (text === "") ask();
-              else if (text !== null) removeSession(text, session.defaultSession)
-            }).catch(console.error);
-          }
-          ask()
-        }
-      }
-      ]
+      label: "Sessions",
+      submenu: [
+        ...getSessionsNames().map((name) => ({
+          label: name,
+          click() {
+            loadSession(name, session.defaultSession);
+          },
+        })),
+        {
+          type: "separator",
+        },
+        {
+          label: "Save Current Session",
+          click: async () => {
+            const ask = () => {
+              prompt({
+                title: "Saving Current Session",
+                label: "Please chose a name:",
+                inputAttrs: {
+                  type: "text",
+                },
+                type: "input",
+              })
+                .then((text) => {
+                  if (text === "") ask();
+                  else if (text !== null) {
+                    storeSession(text, session.defaultSession);
+                    setTimeout(() => loadSession(text, session.defaultSession));
+                  }
+                })
+                .catch(console.error);
+            };
+            ask();
+          },
+        },
+        {
+          label: "Delete A Session",
+          click: async () => {
+            const ask = () => {
+              prompt({
+                title: "Delete A Session",
+                label: "Enter the EXACT NAME to remove:",
+                inputAttrs: {
+                  type: "text",
+                },
+                type: "input",
+              })
+                .then((text) => {
+                  if (text === "") ask();
+                  else if (text !== null)
+                    removeSession(text, session.defaultSession);
+                })
+                .catch(console.error);
+            };
+            ask();
+          },
+        },
+      ],
     },
     {
-      label: 'Theme',
-      submenu: fetchThemes().map(str => ({
+      label: "Theme",
+      submenu: fetchThemes().map((str) => ({
         label: str,
         click() {
           changeUserTheme(str, true);
-        }
+        },
       })),
     },
     {
-      label: 'Options',
+      label: "Options",
       submenu: [
         {
           label: "Streamer mode",
           type: "checkbox",
           checked: loadUserPreferences().streamer,
           click() {
-            toggleStreamer()
-          }
-        }
-      ]
-    }
-  ]
+            toggleStreamer();
+          },
+        },
+      ],
+    },
+    {
+      label: "Settings",
+      submenu: [
+        {
+          label: "Availables AIs",
+          submenu: availableAIs.map(({ label, id, available }) => ({
+            label,
+            type: "checkbox",
+            checked: available === true,
+            click() {
+              toggleAvailableAI(id);
+            },
+          })),
+        },
+        {
+          type: "separator",
+        },
+        {
+          label: "Add a new AI",
+          click: addNewAIPrompt(),
+        },
+      ],
+    },
+  ];
 
   const menu = Menu.buildFromTemplate(sessionMenuTemplate);
   Menu.setApplicationMenu(menu);
+}
+
+function toggleAvailableAI(id) {
+  const aiIndex = availableAIs.findIndex((ai) => ai.id === id);
+  if (aiIndex !== -1) {
+    availableAIs[aiIndex].available = !availableAIs[aiIndex].available;
+    fs.writeFileSync(
+      path.join(__dirname, "settings.json"),
+      JSON.stringify({ availableAIs }, null, 2)
+    );
+    generateMenu();
+  }
+}
+
+function addNewAIPrompt() {
+  return async () => {
+    const aiName = await prompt({
+      title: "Add a new AI",
+      label: "Please enter the AI's name",
+      inputAttrs: {
+        type: "text",
+      },
+      type: "input",
+    });
+
+    if (aiName === null || aiName.trim() === "") return;
+
+    const aiUrl = await prompt({
+      title: "Add a new AI",
+      label: "Please enter the AI website link",
+      inputAttrs: {
+        type: "text",
+      },
+      type: "input",
+    });
+
+    // Should also test if it is a valid link
+    if (aiUrl === null || aiUrl.trim() === "") return; // User cancelled the prompt
+
+    // Find the max id of availableAIs to set the new ai's id
+    const nextId =
+      availableAIs.reduce((maxId, ai) => (ai.id > maxId ? ai.id : maxId), 0) +
+      1;
+
+    availableAIs.push({
+      label: aiName,
+      url: aiUrl,
+      available: true,
+      id: nextId,
+    });
+
+    // Save the updated availableAIs to settings.json
+    fs.writeFileSync(
+      path.join(__dirname, "settings.json"),
+      JSON.stringify({ availableAIs }, null, 2)
+    );
+
+    //Optionally, refresh the menu to include the new AI
+    generateMenu();
+  };
 }
 
 function createWindow() {
@@ -295,24 +396,26 @@ function createWindow() {
       nodeIntegration: true,
       enableRemoteModule: true,
       webviewTag: true,
-      session: session.defaultSession
-    }
+      session: session.defaultSession,
+    },
   });
 
-  win.webContents.session.webRequest.onBeforeSendHeaders((details, callback) => {
-    callback({ cancel: false, requestHeaders: details.requestHeaders });
-  });
+  win.webContents.session.webRequest.onBeforeSendHeaders(
+    (details, callback) => {
+      callback({ cancel: false, requestHeaders: details.requestHeaders });
+    }
+  );
 
   loadUserPreferences();
   generateMenu();
-  const isValidLabel = (label) => availableAIs.find(ai => ai.label === label)
+  const isValidLabel = (label) => availableAIs.find((ai) => ai.label === label);
   const label = isValidLabel(userSettings.assistant)
     ? userSettings.assistant
-    : defaultSettings.assistant
-  const { url } = availableAIs.find(ai => ai.label === label);
+    : defaultSettings.assistant;
+  const { url } = availableAIs.find((ai) => ai.label === label);
   changeAssistant(label, url, false, false);
-  
-  win.webContents.on('did-finish-load', (e) => {
+
+  win.webContents.on("did-finish-load", (e) => {
     generateMenu();
 
     changeUserTheme(userSettings.theme);
@@ -334,20 +437,24 @@ function createWindow() {
         "body div.group.relative.w-full span.relative.flex { opacity: 0 !important; }", // user avatar
         // Copilot
         "body div.absolute.end-6.top-6 button img { opacity: 0 !important; }", // avatar
-        "body div.absolute.end-6.top-6 button p { color: transparent !important; }" // name & mail
+        "body div.absolute.end-6.top-6 button p { color: transparent !important; }", // name & mail
       ];
       for (let cssRule of hideCssRules) win.webContents.insertCSS(cssRule);
     }
   });
 
-  win.on('closed', () => { win = null })
+  win.on("closed", () => {
+    win = null;
+  });
 }
 
-app.commandLine.appendSwitch('disable-software-rasterizer');
+app.commandLine.appendSwitch("disable-software-rasterizer");
 
 app.whenReady().then(createWindow);
 
-app.on('activate', () => { if (win === null) createWindow() })
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("activate", () => {
+  if (win === null) createWindow();
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
